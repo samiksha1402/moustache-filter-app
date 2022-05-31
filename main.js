04821fe1b9bb0f1e23f2fb7f0127e0099163aad5
samiksha1402 <80887833+samiksha1402@users.noreply.github.com>
2022-05-31 21:37:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 nose_y = 0;
 nose_x = 0;
+
+function preload(){
+    mustache_filter = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
+}
+
 function setup(){
     canvas= createCanvas(450, 450);
     canvas.center();
@@ -16,12 +21,13 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
-        nose_x = results[0].pose.nose.x;
-        nose_y = results[0].pose.nose.y;
+        nose_x = results[0].pose.nose.x+62;
+        nose_y = results[0].pose.nose.y+100;
     }
 }
 function draw(){
     image(video,0,0,450,450);
+    image(mustache_filter,nose_x,nose_y,60,50);
 }
 function takesnapshot(){
     save("myimage.png");
